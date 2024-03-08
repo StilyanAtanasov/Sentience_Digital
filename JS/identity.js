a5d37cc395b66bc3259879671521baa4938b1389
Stilyan Atanasov <stilyan2008@gmail.com>
2024-03-08 21:06:10 +0200

    Username Security Added
    Now the user cannot set their username to contain sensitive or offensive words!

--- JS/identity.js
+++ JS/identity.js
@@ -1,12 +1,32 @@
 // Validate the form
 function validateForm(input) {
   let username = input.trim(); // Trim whitespace
-  if (username === "") {
-    // check for empty space
-    alert("Please enter valid input.");
+
+  // Check if the username is whitespace
+  if (username === ``) {
+    alert(`Please enter valid input.`);
     return;
   }
 
+  // Check if the username contains sensitive or offensive words
+  let flaggedWordFound;
+  fetch("../security/Lists of sensitive and offensive words[English].txt")
+    .then((res) => res.text())
+    .then((text) => {
+      const words = text.split("\n").map((word) => word.trim());
+      words.forEach((word) => {
+        if (String(username).includes(word)) {
+          flaggedWordFound = word;
+          alert(
+            `Username cannot contain ${flaggedWordFound}! Please do not include sensitive or offensive word in your username!`
+          );
+          return;
+        }
+      });
+    })
+    .catch((e) => console.error(e));
+
+  if (flaggedWordFound !== null) return;
   setUserName(username);
 }
 
