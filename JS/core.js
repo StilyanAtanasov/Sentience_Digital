245085b9d1ebeb7ac7152d934c40741eda752f48
Stilyan Atanasov <stilyan2008@gmail.com>
2024-03-17 17:33:12 +0200

    Fixing Issue

--- JS/core.js
+++ JS/core.js
@@ -9,14 +9,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: procces.env.FIREBASE_API_KEY,
-  authDomain: procces.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: procces.env.FIREBASE_DATABASE_URL,
-  projectId: procces.env.FIREBASE_PROJECT_ID,
-  storageBucket: procces.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: procces.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: procces.env.FIREBASE_APP_ID,
-  measurementId: procces.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
